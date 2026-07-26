b9a0b3c78dd8657c62da40137610cb62b8a04726
Sagar Saini <Sagar Saini@ho.rnt.ai>
2026-07-26 16:27:31 +0530

    updtae config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-  base: '/<YOUR_REPO_NAME>/', 
+  // Updated base path for GitHub Pages deployment
+  base: '/creative_cv/', 
 })
